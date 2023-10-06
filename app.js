bdf791114340835061f4c94ab9f0074d5f34d621
almpozza <147061376+almpozza@users.noreply.github.com>
2023-10-06 07:57:49 -0300

    alterações realizadas para teste de git pull

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("Commit realizado para testar o git pull")
